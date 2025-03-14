14aba45608e0cf4e30710692b8e3d43f97370848
Suraj_Victus <snkmr573@gmail.com>
2025-03-14 23:49:26 +0530

    added carousal and images in the public folder

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './Components/Navbar';
+import Carousal from './Components/Carousal';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Carousal/>
     </>
   );
 }
--- first-react/src/Components/Navbar.js
+++ first-react/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() { 
         return(
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <div className="container-fluid">
                 <a className="navbar-brand" href="/home">Navbar scroll</a>
                 <button 
