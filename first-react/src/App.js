512ca0dbdc72bf7d4d57ea7d5dcb7c0c7ecabb92
Suraj kumar <er.suraj2022it@gmail.com>
2025-04-24 01:30:07 +0530

    added file upto DOM

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Components/Navbar';
 import Carousal from './Components/Carousal';
 import Footer from './Components/Footer';
 import logo from './logo.svg';
+// import Navbar2 from './Components/Navbar2';
 import './App.css';
 
 
